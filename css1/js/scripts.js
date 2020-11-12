2c4ce8a3a57ed70c4d80a7d74fa4a6c460b0fbe6
F4dr0 <41900341+F4dr0@users.noreply.github.com>
2020-11-13 00:07:13 +0100

    lol

--- css1/js/scripts.js
+++ css1/js/scripts.js
@@ -1,5 +1,8 @@
 var wiek_pabla = parseInt("11");
 
+const heightOutput = document.querySelector('#height');
+const widthOutput = document.querySelector('#width');
+
 const questions_table = [
     ["Ile lat ma Paweł?", wiek_pabla],
     ["Co jest najbardziej niebezpiecznym zwierzęciem w rzece?", "Wąż"],
@@ -20,26 +23,27 @@ function nextNumber() {
     }
     do {
         question_number = Math.random();
-        question_number = Math.floor(question_number * 10) % 4;
+        question_number = Math.floor(question_number * 10) % questions_table.length;
     } while (asked_questions.includes(question_number))
     last_question = question_number;
     return question_number;
 
 }
 
-function checkAnswer(answer){
+function checkAnswer(answer) {
     return answer.valueOf() == questions_table[last_question][1].valueOf();
 }
 
-function getAnswer(){
+function getAnswer() {
     var input = document.getElementById("Answer").innerText;
-    if (checkAnswer(input)){
+    if (checkAnswer(input)) {
         window.alert("You choose wisely!");
         wonsz_get_angery(-0.3);
         asked_questions.push(last_question);
         start();
+        sll
 
-    } else{
+    } else {
         window.alert("You choose poorly!");
         wonsz_get_angery(0.3);
     }
@@ -48,33 +52,60 @@ function getAnswer(){
 
 function wonsz_get_angery(angery) {
     wonsz_angery += angery;
-    var wonsz = document.getElementById("wonsz-jpg");
     document.getElementById("wonsz-jpg").style.opacity = wonsz_angery;
 
-    if (document.getElementById("wonsz-jpg").style.opacity >=1) {
+    if (document.getElementById("wonsz-jpg").style.opacity >= 1) {
         document.writeln("<h1> Przegrałeś! </h1>");
     }
 }
 
+function move_wonsz() {
+    var wonsz = document.getElementById("wonsz-jpg");
+    var pos = Math.random();
+    pos = Math.floor(pos * 10) % 4;
+    switch (pos) {
+        case 0:
+            wonsz.style.top = "51%";
+            break;
+        case 1:
+            wonsz.style.top = "49%";
+            break;
+        case 2:
+            wonsz.style.left = "51%";
+            break;
+        case 3:
+            wonsz.style.left = "49%";
+            break;
+    }
+    wonsz_get_angery(0.003);
+}
 
 function parseAnswer() {
-  document.getElementById("wonszButton").addEventListener("click", getAnswer);
+    document.getElementById("wonszButton").addEventListener("click", getAnswer);
 }
 
 function start() {
-  var question = questions_table[nextNumber()][0];
-  var elem = document.getElementById("question").innerHTML = question;
+    var question = questions_table[nextNumber()][0];
+    var elem = document.getElementById("question").innerHTML = question;
 }
 
 function answer() {
-  var ans = window.prompt("Podaj odpowiedź!");
-  if (ans != null) {
-    document.getElementById("Answer").innerHTML = ans;
-  }
+    var ans = window.prompt("Podaj odpowiedź!");
+    if (ans != null) {
+        document.getElementById("Answer").innerHTML = ans;
+    }
+}
+
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+        currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
 }
 
 function hello() {
-  window.alert("Spotkałeś węża! Aby wąż nie był niebezpieczny musisz odpowiedzieć na kilka zagadek!")
+    window.alert("Spotkałeś węża! Aby wąż nie był niebezpieczny musisz odpowiedzieć na kilka zagadek!")
 }
 
 function nieKlikaj() {
@@ -83,3 +114,4 @@ function nieKlikaj() {
 }
 
 hello();
+window.addEventListener("resize", move_wonsz);
